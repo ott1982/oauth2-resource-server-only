9e0bb151863432e9d5a37c000d862bb9e3dc47aa
Oriol <oriol.tristany@eurecat.org>
2019-09-13 09:09:13 +0200

    initial commit

---
+++ src/main/java/com/orioltristany/oauth2resourceserveronly/Oauth2ResourceServerOnlyApplication.java
@@ -0,0 +1,12 @@
+package com.orioltristany.oauth2resourceserveronly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ResourceServerOnlyApplication {
+
+	public static void main(String... args) {
+		SpringApplication.run(Oauth2ResourceServerOnlyApplication.class, args);
+	}
+}
